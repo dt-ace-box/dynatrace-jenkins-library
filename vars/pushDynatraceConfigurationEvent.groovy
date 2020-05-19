5acdf0a433a520e76226b2f8ed23c107540b2f5f
Andrew Knackstedt <knackstedt@outlook.com>
2020-05-19 15:46:49 -0400

    Created API interface class and modified the pushDynatraceConfiguration script to use it. Not sure if it works.

--- vars/pushDynatraceConfigurationEvent.groovy
+++ vars/pushDynatraceConfigurationEvent.groovy
@@ -1,18 +1,23 @@
+
+
 import groovy.json.JsonOutput
+
+// Why does this look so strange...?
+import ../src/dt_eventAPI
 
 /***************************\
   This function assumes we run on a Jenkins Agent that has curl command available.
 
   Returns either 0(=no errors), 1(=pushing event failed)
 \***************************/
-def call( Map args ) 
-    
-    /*  String dtTenantUrl, 
-        String dtApiToken 
-        def tagRule 
+def call( Map args )
 
-        String description 
-        String source 
+    /*  String dtTenantUrl,
+        String dtApiToken
+        def tagRule
+
+        String description
+        String source
         String configuration
 
         def customProperties
@@ -22,7 +27,7 @@ def call( Map args )
     String dtTenantUrl = args.containsKey("dtTenantUrl") ? args.dtTenantUrl : "${DT_TENANT_URL}"
     String dtApiToken = args.containsKey("dtApiToken") ? args.dtApiToken : "${DT_API_TOKEN}"
     def tagRule = args.containsKey("tagRule") ? args.tagRule : ""
-    
+
     String description = args.containsKey("description") ? args.description : ""
     String source = args.containsKey("source") ? args.source : ""
     String configuration = args.containsKey("configuration") ? args.configuration : ""
@@ -34,41 +39,30 @@ def call( Map args )
         echo "tagRule is a mandatory parameter!"
         return -1
     }
- 
+
     String eventType = "CUSTOM_CONFIGURATION"
 
-    int errorCode = 0
+    return DynatraceAPICall([
+      'dtTenantUrl': dtTenantUrl,
+      'dtApiToken': dtApiToken,
+      'httpMethod': 'POST',
+      'urlPath': 'events',
+      'payload': [
+              eventType: eventType,
+              attachRules: {
+                tagRule: [
+                  {
+                    meTypes: [tagRule[0].meTypes[0].meType]
+                  }
+                ]
+              },
+              tags: tagRule[0].tags,
+              description: description,
+              source: source,
+              configuration: configuration,
+              customProperties: customProperties
+            ],
+      'optionalParameters': []
+      ])
 
-    // build the curl command
-    int numberOfTags = tagRule[0].tags.size()
-    int numberOfProperties = customProperties.size()
-
-    // set Dynatrace URL, API Token and Event Type.
-    String curlCmd = "curl -X POST \"${dtTenantUrl}/api/v1/events?Api-Token=${dtApiToken}\" -H \"accept: application/json\" -H \"Content-Type: application/json\" -d \"{" 
-    curlCmd += " \\\"eventType\\\": \\\"${eventType}\\\","
-    curlCmd += " \\\"attachRules\\\": { \\\"tagRule\\\" : [{ \\\"meTypes\\\" : [\\\"${tagRule[0].meTypes[0].meType}\\\"],"
-
-    // attach tag rules
-    curlCmd += " \\\"tags\\\" : [ "
-    tagRule[0].tags.eachWithIndex { tag, i ->
-        curlCmd += "{ \\\"context\\\" : \\\"${tag.context}\\\", \\\"key\\\" : \\\"${tag.key}\\\", \\\"value\\\" : \\\"${tag.value}\\\" }"
-        if(i < (numberOfTags - 1)) { curlCmd += ", " }
-    }
-    curlCmd += " ] }] },"
-
-    // set description, source, configuration
-    curlCmd += " \\\"description\\\":\\\"${description}\\\", \\\"source\\\":\\\"${source}\\\", \\\"configuration\\\":\\\"${configuration}\\\", "
-
-    // set custom properties
-    curlCmd += " \\\"customProperties\\\": { "
-    customProperties.eachWithIndex { property, i ->
-        curlCmd += "\\\"${property.key}\\\": \\\"${property.value}\\\""
-        if(i < (numberOfProperties - 1)) { curlCmd += ", " }
-    }
-    curlCmd += "} }\" "
-
-    // push the event
-    sh "${curlCmd}"      
-
-    return errorCode
 }
