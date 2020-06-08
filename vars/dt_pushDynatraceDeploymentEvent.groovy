5dbab7c2a36d7df017a629c639861a261d1c4c82
Andrew Knackstedt <knackstedt@outlook.com>
2020-06-08 15:10:48 -0400

    Removed invalid parameter.

--- vars/dt_pushDynatraceDeploymentEvent.groovy
+++ vars/dt_pushDynatraceDeploymentEvent.groovy
@@ -37,41 +37,40 @@ def call( Map args ) {
   // Somehow this runs.
   println dtTenantUrl + '/api/v1/events'
 
-  def http = new HTTPBuilder( dtTenantUrl );
-
-  http.request( POST, JSON ) { req ->
-    uri.path = '/api/v1/events'
+  def http = new HTTPBuilder( dtTenantUrl + '/api/v1/events' );
+  try{
+    http.request( POST, JSON ) { req ->
       headers.'Authorization' = "Api-Token ${dtApiToken}"
       headers.'Content-Type' = 'application/json'
-
       body = [
-          eventType: eventType,
-          attachRules: {
-            tagRule: [{
-              meTypes: [
-                tagRule[0].meTypes[0].meType
-              ]
-            }]
-          },
-          deploymentName: deploymentName,
-          deploymentVersion: deploymentVersion,
-          deploymentProject: deploymentProject,
-          ciBackLink: ciBackLink,
-          remediationAction: remediationAction,
-          tags: tagRule[0].tags,
-          source: "Jenkins"
+      eventType: eventType,
+      attachRules: {
+        tagRule: [{
+          meTypes: [
+            tagRule[0].meTypes[0].meType
+          ]
+        }]
+      },
+      deploymentName: deploymentName,
+      deploymentVersion: deploymentVersion,
+      deploymentProject: deploymentProject,
+      ciBackLink: ciBackLink,
+      remediationAction: remediationAction,
+      tags: tagRule[0].tags,
+      source: "Jenkins"
       ]
-
-
       response.success = { resp, json ->
         echo "Event Posted Successfully! ${resp.status}"
       }
-
       response.failure = { resp, json ->
         echo "Failed To Post Event: " + args.toMapString()
         throw new Exception("Failed to POST Configuration Event. \nargs: \n${args.toMapString()}")
       }
-
+    }
   }
-  println "Why the f*** isn't the requet being sent?"
+  catch(Exception e){
+    echo "[dt_pushDynatraceDeploymentEvent.groovy] SEND EVENT: Exception caught: " + e.getMessage();
+  }
+
+  println "Why the f*** isn't the requet being sent? :L"
 }
