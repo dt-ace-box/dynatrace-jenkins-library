e43f686240580feaae37392ee7af3a4bc27edf56
Andrew Knackstedt <knackstedt@outlook.com>
2020-06-08 14:09:59 -0400

    Removed invalid parameter.

--- vars/dt_pushDynatraceDeploymentEvent.groovy
+++ vars/dt_pushDynatraceDeploymentEvent.groovy
@@ -1,4 +1,4 @@
-@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1' )
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7' )
 
 import groovyx.net.http.HTTPBuilder
 import groovy.json.JsonOutput
